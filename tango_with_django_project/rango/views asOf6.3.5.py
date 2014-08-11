590160e2b17edfdddd8b158406f9395d6206d7af
gzpgg3x <gzpgg3x@yahoo.com>
2014-08-11 15:37:40 -0400

    6.3. Creating a Details Page
    081120140337p

--- tango_with_django_project/rango/views asOf6.3.5.py
+++ tango_with_django_project/rango/views asOf6.3.5.py
@@ -8,21 +8,28 @@ from rango.models import Category, Page
 #     return HttpResponse("Rango says hello world!<a href='/rango/about'>About</a>")
 
 def index(request):
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
     # Obtain the context from the HTTP request.
     context = RequestContext(request)
 
-    # Query for categories - add the list to our context dictionary.
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    # context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    context_dict = {'categories': category_list}    
 
-    # The following two lines are new.
-    # We loop through each category returned, and create a URL attribute.
-    # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
-    for category in category_list:
-        category.url = category.name.replace(' ', '_')
-
-    # Render the response and return to the client.
-    return render_to_response('rango/index.html', context_dict, context)
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    # return render_to_response('rango/index.html', context_dict, context)
+    # Render the response and send it back!
+    return render_to_response('rango/index.html', context_dict, context)    
 
 
 # def index(request):
