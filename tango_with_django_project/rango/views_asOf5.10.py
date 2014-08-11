8f3d9699a6ff70c1272b6f87b56111d54e0d2149
gzpgg3x <gzpgg3x@yahoo.com>
2014-08-11 13:38:00 -0400

    6.2. Showing Categories on Rango’s Homepage
    081120140137p

--- tango_with_django_project/rango/views_asOf5.10.py
+++ tango_with_django_project/rango/views_asOf5.10.py
@@ -10,41 +10,16 @@ from rango.models import Category
 def index(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
-    # Obtain the context from the HTTP request.
     context = RequestContext(request)
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    # context_dict = {'boldmessage': "I am bold font from the context"}
-
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by no. likes in descending order.
-    # Retrieve the top 5 only - or all if less than 5.
-    # Place the list in our context_dict dictionary which will be passed to the template engine.
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}    
+    context_dict = {'boldmessage': "I am bold font from the context"}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    # return render_to_response('rango/index.html', context_dict, context)
-    # Render the response and send it back!
-    return render_to_response('rango/index.html', context_dict, context)    
-
-
-# def index(request):
-#     # Request the context of the request.
-#     # The context contains information such as the client's machine details, for example.
-#     context = RequestContext(request)
-
-#     # Construct a dictionary to pass to the template engine as its context.
-#     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-#     context_dict = {'boldmessage': "I am bold font from the context"}
-
-#     # Return a rendered response to send to the client.
-#     # We make use of the shortcut function to make our lives easier.
-#     # Note that the first parameter is the template we wish to use.
-#     return render_to_response('rango/index.html', context_dict, context)    
+    return render_to_response('rango/index.html', context_dict, context)
 
 # def about(request):
 #     return HttpResponse("Rango Says: Here is the about page.<a href='/rango/'>Index</a>")
